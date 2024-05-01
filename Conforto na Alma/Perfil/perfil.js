b6f428c27eebdd51d81c78462baf1140c4c37acb
Victor Barbosa <126208560+VictorAuBarbosa@users.noreply.github.com>
2024-04-30 21:53:04 -0300

    Add files via upload

--- Conforto na Alma/Perfil/perfil.js
+++ Conforto na Alma/Perfil/perfil.js
@@ -21,11 +21,13 @@ navmenuItems.forEach((item, index) => {
   item.addEventListener('click', () => {
     // Remove a classe ativa de todos os elementos da div navmenu
     navmenuItems.forEach((item) => {
-      item.classList.remove('active');
+      item.classList.remove('selected');
+      item.style.backgroundColor = '#63A355'; // Define a cor padrão para itens não selecionados
     });
 
     // Adiciona a classe ativa ao elemento clicado
-    item.classList.add('active');
+    item.classList.add('selected');
+    item.style.backgroundColor = '#9CE38C'; // Define a cor para o item selecionado
 
     // Oculta todas as seções
     sections.forEach((section) => {
@@ -35,55 +37,75 @@ navmenuItems.forEach((item, index) => {
     // Exibe a seção correspondente ao índice do elemento clicado
     sections[index].style.display = 'flex';
   });
+
+  item.addEventListener('mouseover', () => {
+    // Define a cor de hover para os itens não selecionados
+    if (!item.classList.contains('selected')) {
+      item.style.backgroundColor = '#407935';
+    } else {
+      // Define a cor de hover para os itens selecionados
+      item.style.backgroundColor = '#81c27d';
+    }
+  });
+
+  item.addEventListener('mouseout', () => {
+    // Remove a cor de hover se não estiver selecionado
+    if (!item.classList.contains('selected')) {
+      item.style.backgroundColor = '#63A355';
+    } else {
+      // Remove a cor de hover se estiver selecionado
+      item.style.backgroundColor = '#9CE38C';
+    }
+  });
 });
 
+// Função para remover a classe 'selected' de todos os elementos
+function removeSelectedClass() {
+  var elements = document.querySelectorAll('.menu nav .navmenu div');
+  elements.forEach(function(element) {
+    element.classList.remove('selected');
+    element.style.backgroundColor = '#63A355'; // Define a cor padrão para itens não selecionados
+  });
+}
 
-    // Função para remover a classe 'selected' de todos os elementos
-    function removeSelectedClass() {
-      var elements = document.querySelectorAll('.menu nav .navmenu div');
-      elements.forEach(function(element) {
-          element.classList.remove('selected');
-      });
+// Adicionando um evento de clique a cada elemento
+document.getElementById("dadcontent").addEventListener("click", function() {
+  if (this.classList.contains("selected")) {
+    // Não faz nada se já estiver selecionado
+    return;
+  } else {
+    // Removendo a classe 'selected' de todos os elementos
+    removeSelectedClass();
+    // Adicionando a classe 'selected' apenas ao elemento clicado
+    this.classList.add("selected");
+    // Mudando a cor do elemento dadocad
+    this.style.backgroundColor = "#9CE38C";
   }
+});
 
-  // Adicionando um evento de clique a cada elemento
-  document.getElementById("dadcontent").addEventListener("click", function() {
-      if (this.classList.contains("selected")) {
-          this.classList.remove("selected");
-          this.style.backgroundColor = "#9CE38C"; // restaura a cor original
-      } else {
-          // Removendo a classe 'selected' de todos os elementos
-          removeSelectedClass();
-          // Adicionando a classe 'selected' apenas ao elemento clicado
-          this.classList.add("selected");
-          // Mudando a cor do elemento dadocad
-          this.style.backgroundColor = "#63A355";
-      }
-  });
+document.getElementById("recontent").addEventListener("click", function() {
+  // Mudando a cor do elemento dadocad
+  document.getElementById("dadcontent").style.backgroundColor = "#9CE38C";
+  // Removendo a classe 'selected' de todos os elementos
+  removeSelectedClass();
+  // Adicionando a classe 'selected' apenas ao elemento clicado
+  this.classList.add("selected");
+});
 
-  document.getElementById("recontent").addEventListener("click", function() {
-      // Mudando a cor do elemento dadocad
-      document.getElementById("dadcontent").style.backgroundColor = "#63A355";
-      // Removendo a classe 'selected' de todos os elementos
-      removeSelectedClass();
-      // Adicionando a classe 'selected' apenas ao elemento clicado
-      this.classList.add("selected");
-  });
+document.getElementById("agencontent").addEventListener("click", function() {
+  // Mudando a cor do elemento dadocad
+  document.getElementById("dadcontent").style.backgroundColor = "#9CE38C";
+  // Removendo a classe 'selected' de todos os elementos
+  removeSelectedClass();
+  // Adicionando a classe 'selected' apenas ao elemento clicado
+  this.classList.add("selected");
+});
 
-  document.getElementById("agencontent").addEventListener("click", function() {
-      // Mudando a cor do elemento dadocad
-      document.getElementById("dadcontent").style.backgroundColor = "#63A355";
-      // Removendo a classe 'selected' de todos os elementos
-      removeSelectedClass();
-      // Adicionando a classe 'selected' apenas ao elemento clicado
-      this.classList.add("selected");
-  });
-
-  document.getElementById("hiscontent").addEventListener("click", function() {
-      // Mudando a cor do elemento dadocad
-      document.getElementById("dadcontent").style.backgroundColor = "#63A355";
-      // Removendo a classe 'selected' de todos os elementos
-      removeSelectedClass();
-      // Adicionando a classe 'selected' apenas ao elemento clicado
-      this.classList.add("selected");
-  });
+document.getElementById("hiscontent").addEventListener("click", function() {
+  // Mudando a cor do elemento dadocad
+  document.getElementById("dadcontent").style.backgroundColor = "#9CE38C";
+  // Removendo a classe 'selected' de todos os elementos
+  removeSelectedClass();
+  // Adicionando a classe 'selected' apenas ao elemento clicado
+  this.classList.add("selected");
+});
