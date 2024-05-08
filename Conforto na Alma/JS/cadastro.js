6663205c7282e0459215e81cc67b6ebbf4c2e12c
Álvaro Martins <126208433+arvo-martins@users.noreply.github.com>
2024-05-08 18:59:05 -0300

    Add files via upload

--- Conforto na Alma/JS/cadastro.js
+++ Conforto na Alma/JS/cadastro.js
@@ -1,13 +1,3 @@
-function mostrarCNR() {
-    var cargoSelecionado = document.getElementById("cargo").value;
-    var cnrDiv = document.getElementById("cnrDiv");
-    if (cargoSelecionado === "2") { // Se o cargo selecionado for "Médico" (valor 2)
-        cnrDiv.style.display = "block"; // Mostra o campo CNR
-    } else {
-        cnrDiv.style.display = "none"; // Oculta o campo CNR para outros cargos
-    }
-}
-
 function buscarEndereco() {
     var cep = document.getElementById("cep").value;
     if (cep.length === 8) { // Verifica se o CEP possui 8 dígitos
