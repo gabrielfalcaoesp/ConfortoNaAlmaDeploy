2b3b50f73fa3400a244e4cbb42ae9b4a7ef92724
Álvaro Martins <126208433+arvo-martins@users.noreply.github.com>
2024-05-08 21:20:22 -0300

    Add files via upload

--- Conforto na Alma/JS/pagamento_cartao.js
+++ Conforto na Alma/JS/pagamento_cartao.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const inputs = document.querySelectorAll('.cc-num');
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (e) => {
+            if (input.value.length >= input.maxLength) {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
     const confirmButton = document.querySelector('.confirmButton');
 
     confirmButton.addEventListener('click', function() {
