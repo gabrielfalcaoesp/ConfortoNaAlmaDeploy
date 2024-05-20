0e374e081dcb21b8892a06d8c60a1854bd56592c
Álvaro Martins <126208433+arvo-martins@users.noreply.github.com>
2024-05-20 20:48:47 -0300

    Add files via upload

---
+++ Conforto na Alma/JS/sobre.js
@@ -0,0 +1,37 @@
+function exibirLogin() {
+    fetch('http://localhost:8000/verificarLogin')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Estado da variável:', data);
+        // Aqui você pode fazer o que precisar com o estado da variável (data)
+        
+        var container = document.querySelector('.caixa');
+        var container2 = document.querySelector('.caixa2');
+        
+        if (data != "") {
+          localStorage.setItem('id_usuario', data);
+          container2.style.display = 'block';
+          console.log("O estado do display é: block");
+        } else {
+          container.style.display = 'block';
+          console.log("O estado do display é: block");
+          // Certifique-se de ocultar a caixa2 apenas quando o usuário não estiver logado
+          container2.style.display = 'none';
+        }
+      })
+      .catch(error => console.error('Erro ao verificar estado da variável:', error));
+  }
+  
+  const clicouDentro = document.querySelector('.BL img');
+  const clicouDentroCaixa = document.querySelector('.caixa');
+  const clicouDentroCaixa2 = document.querySelector('.caixa2');
+  document.addEventListener('mousedown', (event) => {
+    if (clicouDentro.contains(event.target) || clicouDentroCaixa.contains(event.target) || clicouDentroCaixa2.contains(event.target)) {
+    } else {
+      var container = document.querySelector('.caixa');
+      var container2 = document.querySelector('.caixa2');
+      container.style.display = 'none';
+      container2.style.display = 'none';
+    }
+  });
+  
